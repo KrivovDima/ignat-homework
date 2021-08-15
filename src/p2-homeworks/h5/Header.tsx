a6d1e3e9ae51d95388b6f0603f565716c0590120
KrivovDima <krivovdima89@gmail.com>
2021-08-15 12:19:15 +0400

    add solution 5 lesson

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styles from './Header.module.css'
+import {PATH} from "./Routes";
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
+        <div className={styles.header}>
+          <ul className={styles.menu}>
+            <li className={styles.menuItem}>
+              <NavLink to={PATH.PRE_JUNIOR} className={styles.link} activeClassName={styles.active}>
+                PreJunior
+              </NavLink>
+            </li>
+            <li className={styles.menuItem}>
+              <NavLink to={PATH.JUNIOR} className={styles.link} activeClassName={styles.active}>
+                Junior {/*Junior 7-11*/}
+              </NavLink>
+
+            </li>
+            <li className={styles.menuItem}>
+              <NavLink to={PATH.JUNIOR_PLUS} className={styles.link} activeClassName={styles.active}>
+                Junior+
+              </NavLink>
+            </li>
+          </ul>
 
         </div>
     )
